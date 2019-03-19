a91080999f37ea2b0c3ce3ed5dc1f95459429d61
aguir <aguirrym3@gmail.com>
2019-03-19 09:01:36 +0100

    inital commit

---
+++ GestionDordreDeMission.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using GestionDordreDeMission.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionDordreDeMission.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+        
+
+        public DbSet<Employee> Employees {get; set;}
+    }
+}
---
+++ GestionDordreDeMission.API/Models/Employee.cs
@@ -0,0 +1,11 @@
+namespace GestionDordreDeMission.API.Models
+{
+    public class Employee
+    {
+        public int Id {get; set;}
+        public string NumPasseport{get; set;}
+        public string Prenom{get; set;}
+        public string Nom{get; set;}
+        public string PostEmp{get; set;}
+    }
+}
